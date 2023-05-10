f7cc53931ea1c6de07ae828818fa59ea68dfe198
leoparolo <leonardoparolo80@gmail.com>
2023-05-10 07:15:16 -0300

    v1.0.0.2 / Completado formato de paginas 'Como funciona' y 'Acerca de' / footer / background-content

---
+++ js/tab.js
@@ -0,0 +1,15 @@
+document.getElementById("defaultOpen").click();
+
+function openContent(evt, content) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(content).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
